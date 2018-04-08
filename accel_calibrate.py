b87495ee5115d1821201d621d4d78693fb7fe9f8
Andrew VanSchalkwyk <drewsv22@gmail.com>
2018-04-07 22:26:21 -0400

    Working on drone script.

--- accel_calibrate.py
+++ accel_calibrate.py
@@ -23,7 +23,7 @@ def calc_norm_accel():
 
 time_start = time.time()
 
-for x in range(0, int(seconds / loop_delay)):
+for x in range(0, math.trunc(seconds / (1/loop_delay))):
     calc_norm_accel()
     accXnorms += accXnorm
     accYnorms += accYnorm
