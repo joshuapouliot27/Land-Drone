88e9c8b07e637c4e4f4d0a4dc4f2cc1878b97eb2
Joshua Pouliot <joshuapouliot27@gmail.com>
2018-04-15 01:10:37 -0400

    Working on drone controlling script.

--- heading_test.py
+++ heading_test.py
@@ -1,3 +1,4 @@
+import math
 import time
 
 from Heading_Calculator import Heading_Calculator
@@ -7,7 +8,14 @@ from LSM6DS33 import LSM6DS33
 magn = LIS3MDL()
 acc_gyr = LSM6DS33()
 calc = Heading_Calculator(acc_gyr, magn)
+points = set()
+max_points = 5
 while True:
-    heading = calc.calculate_tilt_compensated_heading()
+    if len(points) >= max_points:
+        for point in points:
+            points.remove(point)
+            break
+    points.add(calc.calculate_tilt_compensated_heading())
+    heading = math.fsum(points) / len(points)
     print("Heading: "+str(heading))
     time.sleep(1/10)
