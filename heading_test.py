e27c6c1422f7dc0db6e46445563181945bdb2e0a
Joshua Pouliot <joshuapouliot27@gmail.com>
2018-04-16 16:41:26 -0400

    Revert "Working on drone controlling script."

--- heading_test.py
+++ heading_test.py
@@ -7,21 +7,41 @@ from Background_Thread import Background_Thread
 from imu_fusion import imu_fusion
 from LIS3MDL import LIS3MDL
 from LSM6DS33 import LSM6DS33
+import RTIMU
 
-magn = LIS3MDL()
-acc_gyr = LSM6DS33()
-calc = HeadingCalculator(acc_gyr, magn)
+setting_file = "./RTIMULib.ini"
+rtimu = RTIMU.Settings(setting_file)
+imu = RTIMU.RTIMU(rtimu)
+if not imu.IMUInit():
+    print("IMU init failed!")
+    exit(1)
+# magn = LIS3MDL()
+# acc_gyr = LSM6DS33()
+# calc = HeadingCalculator(acc_gyr, magn)
 # fus = imu_fusion(acc_gyr, magn)
-points = set()
-max_points = 5
+# points = set()
+# max_points = 5
 # thread = Background_Thread(fus.loop)
+imu.setSlerpPower(0.02)
+imu.setGyroEnable(True)
+imu.setAccelEnable(True)
+imu.setCompassEnable(True)
+poll_interval = imu.IMUGetPollInterval()
 while True:
-    if len(points) >= max_points:
-        for point in points:
-            points.remove(point)
-            break
-    points.add(calc.calculate_tilt_compensated_heading())
+    # if len(points) >= max_points:
+    #     for point in points:
+    #         points.remove(point)
+    #         break
+    # points.add(calc.calculate_tilt_compensated_heading())
     # points.add(fus.get_true_heading())
-    heading = math.fsum(points) / len(points)
+    # heading = math.fsum(points) / len(points)
+    data = imu.getIMUData()
+    fusionPose = data["fusionPost"]
+    roll_rad = fusionPose[0]
+    pitch_rad = fusionPose[1]
+    yaw_rad = fusionPose[2]
+    x = math.cos(yaw_rad) * math.cos(pitch_rad)
+    y = math.sin(yaw_rad) * math.cos(pitch_rad)
+    heading = math.degrees(math.atan2(y, x))
     print(time.strftime("%X") + "; Heading: " + str(heading))
     time.sleep(1 / 2)
