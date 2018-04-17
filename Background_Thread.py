c0d7dbf7c261fe7140af967314ae0b162c37830f
Joshua Pouliot <joshuapouliot27@gmail.com>
2018-04-16 23:27:17 -0400

    Revert "Working on drone controlling script."

--- Background_Thread.py
+++ Background_Thread.py
@@ -4,16 +4,16 @@ import time
 
 class Background_Thread(object):
 
-    def __init__(self, function, arguments=()):
+    def __init__(self, function, arguments=None):
         self.function = function
         self.arguments = arguments
 
-        thread = threading.Thread(target=self.run, args=arguments)
+        thread = threading.Thread(target=self.run, args=())
         thread.daemon = True                            # Daemonize thread
         thread.start()                                  # Start the execution
 
     def run(self):
-        if len(self.arguments) > 0:
+        if self.arguments is not None:
             self.function(*self.arguments)
         else:
             self.function()
